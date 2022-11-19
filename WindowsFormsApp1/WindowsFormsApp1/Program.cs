0f9e18cf4402a515e96af648c5211a6323005482
logunov28 <79602109+logunov28@users.noreply.github.com>
2022-11-19 18:35:51 +0300

    Убрал js, добавил MessageBox'ы

--- WindowsFormsApp1/WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Diagnostics;
 
 namespace WindowsFormsApp1
 {
@@ -14,9 +16,35 @@ namespace WindowsFormsApp1
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new NoPython());
+            if (Registry.CurrentUser.OpenSubKey(@"Software\Python") != null) //если путь существует
+            {
+                if (System.IO.File.Exists("run.pyc"))
+                {
+                    Process.Start("run.pyc");
+                }
+                else if (System.IO.File.Exists("run.pyo"))
+                {
+                    Process.Start("run.pyo");
+                }
+                else if (System.IO.File.Exists("run.pyw"))
+                {
+                    Process.Start("run.pyw");
+                }
+                else if (System.IO.File.Exists("run.py"))
+                {
+                    Process.Start("run.py");
+                }
+                else
+                {
+                    MessageBox.Show("Nothing to run!", "Error");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Python 3 is not installed!", "Error");
+            }
+            
         }
     }
 }
--- winerror.js
+++
@@ -1,8 +0,0 @@
-// встроенный объект
-var WshShell = WScript.CreateObject("WScript.Shell");
-
-// выводим сообщение
-WshShell.Popup("Python 3 is not installed!", 0, "Error");
-
-// завершаем работу с объектом
-WScript.Quit();
